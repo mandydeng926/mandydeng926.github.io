dd75336b74b546ac724effbc1b55b45b247724a3
mandydeng <mandydeng926@163.com>
2023-10-04 19:35:51 +0800

    version1

---
+++ data/global.js
@@ -0,0 +1,72 @@
+window.global_data = {
+  working_projects: [
+    {
+      title: "Hiding under the table: Transparency of executive hedging and firm performance",
+      circulate: "Presented at:2023 Singapore Rising Scholars Conference at SMU (Best Paper Award), The University of Hong Kong, Central University of Finance and Economics (on schedule)",
+      abstract:
+        "This study explores the impact of the Securities and Exchange Commission’s (SEC) mandatory disclosure rule for executive hedging on firms’ economic performance. Using the novel textual data about executive hedging policies and practices from proxy statements, I find the positive impact of the mandate on firms’ performance and shareholder value. The transparency of executive hedging would increase the cost of hedging and restore the interest alignment, leading to better economic outcomes. The cross-sectional analysis shows that the positive impact of the mandate on firm performance is more salient for firms with higher incentives of strategic hedging, lower investment efficiency, and a less transparent information environment ex-ante. The research sheds light on the limited study of executive hedging and demonstrates the bright side of the SEC’s mandate.",
+      authors: "Mengdie Deng (HKU)",
+      status: "Job Market Paper",
+    },
+    {
+      title: "Managerial forward-looking orientation and corporate environmental risk: Evidence from a machine learning approach",
+      circulate: "Presented at:2023 GRASFI Annual Conference at Yale (Award for the Most Impactful Research), The University of Hong Kong",
+      abstract:
+        "The paper investigates whether managers with forward-looking tendencies are more likely to reduce corporate environmental risk. To answer this question, we adopt a machine learning approach to analyze Q&A sessions of Earnings Conference Calls and create a forward-looking orientation index at the firm-year level. We find that managers’ forward-looking tendencies are associated with subsequent lower corporate environmental risks. The relationship is stronger when managers are confronted with lower present bias and their firms receive more attention from socially responsible investors and other stakeholders. We also show that firms with forward-looking managers reduce more Greenhouse Gas emissions when mitigating environmental risks. Our results are robust to using alternative specifications for index construction, adopting the instrumental variable approach, and controlling for future performance and traditional proxies for managerial myopia.",
+      authors: "Mengdie Deng (HKU), Tse-Chun Lin (HKU) and Jiayu Zhou (HKU)",
+      status: "",
+    },
+    {
+      title: "Climate policy and firm valuation: Evidence from California Cap-and-Trade Program ",
+      circulate: "",
+      abstract:
+        "This paper examines the impact of a market-based climate policy, the California cap-and-trade program, on individual firms’ valuation. Counter to the intuition that environmental policies commonly impose compliance costs, we find that the program in general increases affected firms’ value. We document that the impact on firm valuation is more salient when firms have higher exposure to the program. To further illustrate the mechanism, we examine the cross-sectional variations on green versus brown firms. We find that after the program begins, green firms generate higher revenues and profitability, implying the sales of extra carbon allowances. Together, our paper extends the understanding of the economic consequence of modern climate policies.",
+      authors: "Mengdie Deng (HKU), Kaiwai Hui (HKU) and Eric Yeung (Cornell University)",
+      date: "10 2023",
+      status: "",
+    },
+    {
+      title: "The invisible hand in the dark: The disciplinary effect of dark trading on firm overinvestment",
+      circulate: "Presented at: 2022 Financial Management Association (FMA) Annual Meeting, 13th The Japanese Accounting Review (TJAR) Conference, 2022 New Zealand Finance Meeting ",
+      abstract:
+        "We propose that the availability of dark trading disciplines managers for their investment decisions. Utilizing the trade-at-rule provision in the SEC’s Tick Size Pilot Program, we find that restrictions on dark trading lead to higher levels of corporate overinvestment for medium- and small-cap securities. As dark trading incentivizes informed traders to acquire information ex-ante, we also find the results are more pronounced for firms with a larger short-selling flow before the Program. As a consequence, overinvestment due to the restrictions in dark trading worsens the firms’ future performance. Overall, we identify a novel external governance mechanism via dark trading venues.",
+      authors: "Mengdie Deng (HKU), Tse-Chun Lin (HKU) and Jiayu Zhou (HKU)",
+      date: "10 2023",
+      status: "Under Review",
+    },
+    {
+      title: "Takeover risk and boundary of technology diffusion: Evidence from licensing contract design",
+      circulate: "Presented at: The University of Hong Kong ",
+      abstract:
+        "The paper studies whether and how takeover risk affects the design of the licensing contract. While the takeover risk of a licensee may improve the value of the licensing contract through technological diffusion and commercialization, it also arouses licensors’ concerns about unwanted technology diffusion. The licensor thus trades off the cost and benefit in drafting the assignment clause in the contract. Based on the textual analysis of the license contract, we find a robust positive association between expected downstream takeover risk and the usage of restrictive assignment clauses that retain the licensor’s decision right about the scope of technology diffusion, suggesting the dominance of proprietary costs concerns. The association is more pronounced when licensors are confronted with greater proprietary cost concerns, but is weakened when the licensee operates in states with strong anti-takeover laws and when it is more important to motivate licensees’ relationship-specific investment. Consistent with takeover risk enhancing the contract value through technology diffusion, we find that the restrictive assignment clauses are associated with negative market reactions of licensees.",
+      authors: "Mengdie Deng (HKU), Kaiwai Hui (HKU) and Guoman She (HKU) ",
+      status: "Ready for Submission",
+    },
+  ],
+
+  publication: [
+    {
+      title: "Does better liquidity for large orders attract institutional investors and analysts? Evidence from the Tick Size Pilot Program",
+      abstract:
+        "Based on SEC’s Tick Size Pilot Program, we adopt a difference-in-difference design and find that the improved liquidity for large orders increases their ownership of the treated firms with a larger tick size during the program. The effect is concentrated among firms with lower liquidity for large orders ex-ante and mainly comes from dedicated investors and quasi-indexers. We also find that analyst coverage and forecast accuracy increase for the treated firms, plausibly catering to the increased information demand of institutional investors. As a consequence, price efficiency increases as well. Overall, we show the bright side of the controversial Program. ",
+      circulate:"",
+      authors: "Mengdie Deng (HKU), Tse-Chun Lin (HKU) and Jiayu Zhou (HKU)",
+      status: "Accepted by The Journal of Financial Markets",
+      href: ""
+    },
+  ],
+  teaching: [
+    {
+      title: "Introduction to Financial Accounting",
+      authors:"undergraduate",
+      abstract: "Valuation score 95.3/100",
+      circulate: "Teaching Assistant (with turorials)",
+    },
+    {
+      title: "Statistical Analysis",
+      authors:"Graduate",
+      abstract: "Teaching Assistant (with turorials)",
+      circulate: "",
+    },
+  ],
+};
---
+++ js/main.js
@@ -0,0 +1,42 @@
+class ProjectCard extends HTMLElement {
+    constructor(){
+      super()
+    }
+    connectedCallback(){
+      var shadow = this.attachShadow( { mode: 'closed' } );
+      var templateElem = document.getElementById('ProjectCardTemplate');
+      var content = templateElem.content.cloneNode(true);
+      
+      content.querySelector('.authors').innerText = this.authors;
+    //   content.querySelector('.article-date').innerText = this.email;
+      content.querySelector('.section-subheading').innerText = this.title;
+      content.querySelector('.circulate').innerText = this.circulate;
+      content.querySelector('.abstract').innerText = this.abstract;
+      content.querySelector('.status').innerText = this.status;
+      content.querySelector('.pub').href = this.href || '';
+      shadow.appendChild(content);
+    }
+  }
+window.customElements.define('project-card', ProjectCard);   
+
+function createComponentsList(targetElementID, data){
+    const target = document.getElementById(targetElementID);
+    list = data.map( item => {
+        const el = document.createElement("project-card");
+        el.title = item.title;
+        el.circulate = item.circulate;
+        el.authors = item.authors;
+        el.status = item.status;
+        el.abstract = item.abstract;
+        el.href = item.href || '';
+        return el;
+    });
+    console.log(list);
+    target.append.apply(target, list);
+}
+
+const { working_projects, teaching, publication } = window.global_data;
+
+createComponentsList('projects_list', working_projects);
+createComponentsList('publication_list', publication);
+createComponentsList('teaching_list', teaching);
